7734db2a7f9428763f9e72f46d5fc10e16a7cb82
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-06-14 12:23:42 +0300

    completed the login endpoint and tested successfully

---
+++ EquityAfia.UserManagement.Api/Controllers/Authentication/Login/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using EquityAfia.UserManagement.Application.Authentication.Commands.Register.RegisterPractitioner;
+using EquityAfia.UserManagement.Application.Authentication.Queries.LogIn;
+using EquityAfia.UserManagement.Contracts.Authentication;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EquityAfia.UserManagement.Api.Controllers.Authentication.Login
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+        public LoginController(IMapper mapper, IMediator mediator)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            var query = new LoginQuery(loginRequest);
+
+            var logIn = await _mediator.Send(query);
+
+            var mappedResponse = _mapper.Map<LoginResponse>(logIn);
+
+            return Ok(mappedResponse);
+        }
+    }
+}
---
+++ EquityAfia.UserManagement.Contracts/Authentication/LoginResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquityAfia.UserManagement.Contracts.Authentication
+{
+    public class LoginResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Token { get; set; }
+    }
+}
