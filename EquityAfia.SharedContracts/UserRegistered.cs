68e5a23a514c95f3cd25820cfacb4b0728cb0c63
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-02 10:09:23 +0300

    Added a shared project for messaging and communications

---
+++ EquityAfia.SharedContracts/UserRegistered.cs
@@ -0,0 +1,11 @@
+﻿namespace EquityAfia.SharedContracts
+{
+    public class UserRegistered
+    {
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
