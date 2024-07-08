6d12b2cf5b65ae7f62a47def49a48524e360f1d2
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-07-08 12:26:59 +0300

    added user exists event

---
+++ EquityAfia.UserManagement.Application/EventsRequestsHandlers/UserExistsRequestHandler.cs
@@ -0,0 +1,37 @@
+﻿using EquityAfia.SharedContracts;
+using EquityAfia.UserManagement.Application.Interfaces;
+using MassTransit;
+
+namespace EquityAfia.UserManagement.Application.EventsRequestsHandlers
+{
+    public class UserExistsRequestHandler : IConsumer<UserExists>
+    {
+        public readonly IUserRepository _userRepository;
+        public readonly UserExists _userExists;
+
+        public UserExistsRequestHandler(IUserRepository _userRepository, UserExists userExists)
+        {
+            _userRepository = _userRepository;
+            _userExists = userExists;
+        }
+        public async Task Consume(ConsumeContext<UserExists> context)
+        {
+            var user = await _userRepository.GetUserByIdAsync(_userExists.IdNumber);
+
+            if (user is null)
+            {
+                throw new Exception("User not found");
+            }
+
+            var response = new UserExists
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                IdNumber = user.IdNumber,
+            };
+
+            //return response;
+        }
+    }
+}
---
+++ EquityAfia.UserManagement.Infrastructure/Consumers/UserExistsConsumer.cs
@@ -0,0 +1,18 @@
+﻿using EquityAfia.SharedContracts;
+using MassTransit;
+
+namespace EquityAfia.UserManagement.Infrastructure.Consumers
+{
+    public class UserExistsConsumer : IConsumer<UserExists>
+    {
+        public async Task Consume(ConsumeContext<UserExists> context)
+        {
+
+            Console.WriteLine($"Checking if user exist ...");
+
+            await Task.Delay(1000); 
+
+            Console.WriteLine("User found successfully.");
+        }
+    }
+}
